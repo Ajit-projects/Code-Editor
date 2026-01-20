bbf2376cc7cff08c277a1f1eabd948ca5b8d9b16
Ajit-projects <abjitak9822@gmail.com>
2026-01-20 22:51:19 +0530

    updated config file

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: {},
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
