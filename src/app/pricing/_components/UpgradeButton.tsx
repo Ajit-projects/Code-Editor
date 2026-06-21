aba2b937f7dbe38cab4bd59a2bc7b03be873583d
Ajit-projects <abjitak9822@gmail.com>
2026-06-21 13:25:35 +0530

    Fixed pricing page issue

--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -2,7 +2,7 @@ import { Zap } from "lucide-react";
 import Link from "next/link";
 
 export default function UpgradeButton() {
-  const CHEKOUT_URL = process.env.PAYMENT_URL!;
+  const CHEKOUT_URL = process.env.NEXT_PUBLIC_PAYMENT_URL!;
 
   return (
     <Link
